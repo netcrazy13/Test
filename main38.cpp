13c20e93895ac5e7723104359ff578f15957615d
netcrazy13 <netcrazy.n@gmail.com>
2015-12-12 17:39:18 +0700

    Complete Solution

---
+++ main38.cpp
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <conio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+main() {
+int a,b;
+
+printf("Enter number : ");
+scanf("%d",&a);
+b =a*a;
+printf("the square is : %d",b);
+getch();
+
+}
